8e0293c6a4410596ff8debffcf0a1db750267026
Chris Mabry <cjmab28@gmail.com>
2017-08-06 22:41:57 -0500

    representative models and rough scorecard

---
+++ summerforprogress/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from models import *
+
+@admin.register(Representative)
+class RepresentativeAdmin(admin.ModelAdmin):
+    exclude = ()
+    
+    list_display = ('first','last','score','state','district','healthcare','college_tuition','minimum_wage','workers_rights','womens_rights','voting_rights','justice','climate','last_updated')
+    
+    search_fields = ('first','last','state','district')
