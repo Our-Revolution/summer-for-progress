2c4f00832e62c4aaf81018c2bd7e1223a306b0c7
Chris Mabry <cjmab28@gmail.com>
2017-08-08 11:18:36 -0500

    disaloow delete on reps

--- summerforprogress/admin.py
+++ summerforprogress/admin.py
@@ -9,3 +9,10 @@ class RepresentativeAdmin(admin.ModelAdmin):
     list_display = ('first','last','score','state','district','healthcare','college_tuition','minimum_wage','wall_street','womens_rights','voting_rights','justice','climate','last_updated')
     
     search_fields = ('first','last','state','district')
+    
+    readonly_fields = ('score',)
+
+    def get_actions(self, request):
+        actions = super(RepresentativeAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
